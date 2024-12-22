26b7a805abb316a202ad15765af73497816707b9
Amit Pandey <amitpande184@gmail.com>
2024-12-22 18:13:41 +0530

    Create Choose  VehicalPanel

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -9,8 +9,10 @@ const Home = () => {
   const [pikup, setPinkup] = useState();
   const [destination, setDestination] = useState();
   const [panelOpen, setPanelOpen] = useState(false);
+  const [VehicalPanelOpen ,setVehicalPanelOpen] = useState(false)
   const panelRef = useRef(null);
-  const panleClodeRef = useRef(null);
+  const panleCloseRef = useRef(null);
+  const VehicalPanelRef = useRef(null);
   const submitHandeler = (e) => {
     e.preventDefault();
   };
@@ -18,29 +20,43 @@ const Home = () => {
     if (panelOpen) {
       gsap.to(panelRef.current, {
         height: "70%",
-        paddingTop:12
+        padding:12
       })
-      gsap.to(panleClodeRef.current, {
+      gsap.to(panleCloseRef.current, {
         display: 'block',
       })
     } else {
       gsap.to(panelRef.current, {
         height: "0%",
+        padding:0
       })
-      gsap.to(panleClodeRef.current, {
+      gsap.to(panleCloseRef.current, {
         display: 'none',
       })
     }
   }, [panelOpen]);
 
+  useGSAP(()=>{
+    if(VehicalPanelOpen){
+      gsap.to(VehicalPanelRef.current,{
+        transform:'translateY(0%)'
+      })
+    }
+    else{
+      gsap.to(VehicalPanelRef.current,{
+        transform:'translateY(100%)'
+      })
+    }
+  },[VehicalPanelOpen])
+
   return (
-    <div className=" h-screen  w-full relative bg-coverbg-center bg-[url(https://i.pinimg.com/736x/22/1b/42/221b42fa58ead3ffba876fb0d24cc113.jpg)]">
+    <div className=" h-screen  w-full relative bg-coverbg-center bg-[url('https://i.pinimg.com/736x/22/1b/42/221b42fa58ead3ffba876fb0d24cc113.jpg')]">
       <div className="w-full flex justify-center items-center  relative  z-[8]">
         <img className="w-[8rem] filter invert" src={logo} alt="" />
       </div>
-      <div className="h-screen  w-full absolute flex flex-col  justify-end  top-0 left-0 px-4   bg-white  ">
-        <div className=" h-[35%] w-full bg-white  rounded-tl-md rounded-tr-md  py-2 flex flex-col  gap-2 relative">
-            <h5 ref={panleClodeRef} onClick={() => {setPanelOpen(false)}} className="text-xl z-[9]  inline-block relative  w-fit "><i className="ri-arrow-down-wide-line inline-block "></i></h5>
+      <div className="h-screen  w-full absolute flex flex-col  justify-end  top-0 left-0      ">
+        <div className=" h-[35%] w-full bg-white  rounded-tl-md rounded-tr-md  px-4 py-2 flex flex-col  gap-2 relative">
+            <h5 ref={panleCloseRef} onClick={() => {setPanelOpen(false)}} className="text-xl z-[9]  inline-block relative  w-fit "><i className="ri-arrow-down-wide-line inline-block "></i></h5>
             <h1 className="text-2xl font-bold text-black">Find a trip</h1>
           <form
             onSubmit={(e) => submitHandeler(e)}
@@ -71,10 +87,72 @@ const Home = () => {
             </div>
           </form>
         </div>
-        <div ref={panelRef} className="h-0 w-full bg-[#dadada] overflow-x-auto ">
-          <LocataionSearchPanel/>
+        <div ref={panelRef} className="h-0 w-full bg-white overflow-x-auto  ">
+          <LocataionSearchPanel   setPanelOpen={setPanelOpen} setVehicalPanel={setVehicalPanelOpen} />
         </div>
       </div>
+      <div  ref={VehicalPanelRef} className="w-full fixed translate-y-full  bottom-0 bg-gray-100 flex justify-center items-center z-[999] ">  
+      <div className="w-full max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
+        <div className=" text-black text-lg text-start p-4 border-b-2 font-semibold">
+           Choose Vehical
+        </div>
+
+        <div className="p-4">
+          <div className="border active:border-black  rounded-lg mb-4  p-2">
+            <div className="flex justify-between items-center   ">
+              <div className="img h-12 w-16 ">
+                <img className="h-full   w-full object-cover" src="https://i.pinimg.com/736x/5c/be/1d/5cbe1d84b6d750fe1847003cbf9af6cd.jpg" alt="" />
+              </div>
+              <div className="leading-tight">
+              <h3 className="text-sm font-semibold">Buddy Go <span><i className="ri-user-fill"></i> 3</span></h3>
+              <p className="text-[4vw] text-gray-500">2 min away • 3 seats</p>
+              <p className="text-[4vw] text-gray-500">Affordable, Compact rides</p>
+              </div>
+              <div className="text-right">
+                <p className="text-sm font-semibold text-green-600">₨ 100</p>
+                <p className="text-sm line-through text-gray-400">₨ 130</p>
+              </div>
+            </div>
+          
+          </div>
+
+          <div className="border active:border-black  rounded-lg mb-4 p-2">
+            <div className="flex justify-between items-center">
+              <div className="img h-12 w-16  ">
+                <img className=" w-full h-full object-cover" src="https://i.pinimg.com/736x/0f/b0/b0/0fb0b022e23209f8081e40edf62cc9f0.jpg" alt="" />
+              </div>
+              <div className="leading-tight">
+              <h3 className="text-sm font-semibold">Buddy Go <span><i className="ri-user-fill"></i> 1</span></h3>
+              <p className="text-[4vw] text-gray-500">2 min away • 1 seats</p>
+              <p className="text-[4vw] text-gray-500">Affordable, Compact rides</p>
+              </div>
+              <div className="text-right">
+                <p className="text-sm font-semibold text-green-600">₨ 64</p>
+                <p className="text-sm line-through text-gray-400">₨ 130</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="border  active:border-black  rounded-lg  p-2">
+            <div className="flex justify-between items-center">
+            <div className="img h-12 w-16  ">
+                <img className=" w-full h-full object-contain" src="https://i.pinimg.com/736x/d7/ee/49/d7ee492124189c6cdc08750eaa39a418.jpg" alt="" />
+              </div>
+              <div className="leading-tight">
+              <h3 className="text-sm font-semibold">Buddy Go <span><i className="ri-user-fill"></i> 4</span></h3>
+              <p className="text-[4vw] text-gray-500">2 min away • 5 seats</p>
+              <p className="text-[4vw] text-gray-500">Affordable, Compact rides</p>
+              </div>
+              <div className="text-right">
+                <p className="text-sm font-semibold text-green-600">₨ 120</p>
+                <p className="text-sm line-through text-gray-400">₨ 150</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     </div>
   );
 };
--- frontend/src/components/LocataionSearchPanel.jsx
+++ frontend/src/components/LocataionSearchPanel.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 
-const LocataionSearchPanel = () => {
+const LocataionSearchPanel = (props) => {  
+  const  location = [
+    {
+    location: '24B, near kapoor\'s cafe ,Sheriyan Codeing Scholl , bhopal',
+    },
+    {
+      location: '14B,T-Satal , near Andand Nager  , bhopal',
+    },
+    {
+      location: '24B, near mohan\'s cafe ,  bhopal',
+    }
+  
+  ] 
+
+
   return (
     <div className='flex flex-col justify-start  items-start gap-5'>
-      <div className='flex justify-start items-center gap-4'>
-        <h2 className='bg-[#eee] w-16 h-10 rounded-full flex justify-center items-center'><i className="ri-map-pin-line"></i></h2>
-        <h2 className='font-medium tracking-tighter leading-tight'>24B, near kapoor's cafe ,Sheriyan Codeing Scholl , bhopal</h2>
-      </div>
+       {location.map((data,index) =>{
+        return(
+         <div
+          key={index}
+          onClick={
+            () => {
+              props.setVehicalPanel(true)
+              props.setPanelOpen(false)
+            }
+          }
+         className='flex w-full justify-start items-center gap-4 active:border-[#222] border p-2 rounded-lg '>
+          <h2 className='bg-[#eee]  rounded-full px-3 py-2 flex justify-center items-center'>
+            <i className="ri-map-pin-line text-[5vw]"></i>
+          </h2>
+          <h2 className='font-medium tracking-tighter leading-tight'>{data.location}</h2>
+          </div>
+        )
+       })}
+
+     
+   
      
      
     </div>
